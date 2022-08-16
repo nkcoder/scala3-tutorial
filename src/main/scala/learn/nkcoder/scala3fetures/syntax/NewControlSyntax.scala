cc395672be0a9176334ec17eaeaab14a8199c64c
Ling Guo <ling.guo@rea-group.com>
2022-08-16 08:19:54 +0800

    update the description of rules

--- src/main/scala/learn/nkcoder/scala3fetures/syntax/NewControlSyntax.scala
+++ src/main/scala/learn/nkcoder/scala3fetures/syntax/NewControlSyntax.scala
@@ -3,16 +3,9 @@ package learn.nkcoder.scala3fetures.syntax
 /**
  * Scala 3 has a new "quiet" syntax for control expressions that does not rely on enclosing the condition in
  * parentheses, and also allows to drop parentheses or braces around the generators of a for-expression.
- *
- * Rules:
- * - The condition of an if-expression can be written without enclosing parentheses if it is followed by a then.
- * - The condition of a while-loop can be written without enclosing parentheses if it is followed by a do.
- * - The enumerators of a for-expression can be written without enclosing parentheses or braces if they are followed
- * by a yield or do.
- * - A do in a for-expression expresses a for-loop.
  */
 object NewControlSyntax extends App :
-  // The condition of an if-expression can be written without enclosing parentheses if it is followed by a then.
+  // Rule 1: the condition of an if-expression can be written without enclosing parentheses if it is followed by a then.
   def ifFunc(x: Int): String =
     if x < 0 then
       "negative"
@@ -26,14 +19,14 @@ object NewControlSyntax extends App :
   def ifFunc2WithParentheses(x: Int): String = if (x < 0) "negative" else if (x == 0) "zero" else "positive"
 
 
-  // The condition of a while-loop can be written without enclosing parentheses if it is followed by a do.
+  // Rule 2: the condition of a while-loop can be written without enclosing parentheses if it is followed by a do.
   // do...while is dropped in scala 3
   def whileDoFunc(x: Int) = while x > 0 do ifFunc(x)
 
   def whileDoFuncWithParentheses(x: Int) = while (x > 0) ifFunc(x)
 
 
-  // The enumerators of a for-expression can be written without enclosing parentheses or braces if they are followed
+  // Rule 3: the enumerators of a for-expression can be written without enclosing parentheses or braces if they are followed
   // * by a yield or do.
   def forYieldFunc(xs: List[Int]): List[Int] = for x <- xs if x > 0 yield x * x
 
