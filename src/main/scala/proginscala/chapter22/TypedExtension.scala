51ccb139ba05cee1e52fe66bed705ca72594ff1a
Ling Guo <ling.guo@rea-group.com>
2023-01-01 15:55:48 +0800

    chapter 22: done

--- src/main/scala/proginscala/chapter22/TypedExtension.scala
+++ src/main/scala/proginscala/chapter22/TypedExtension.scala
@@ -26,14 +26,36 @@ object TwosComplement:
     def absOf(n: Long) = n.abs
     def negationOf(n: Long) = (-n).toLong
 
-extension [N](n: N)(using tc: TwosComplement[N])
-  def isMinValue: Boolean = tc.equalsMinValue(n)
-  def absOption: Option[N] =
-    if !isMinValue then Some(tc.absOf(n)) else None
-  def negateOption: Option[N] =
-    if !isMinValue then Some(tc.negationOf(n)) else None
+/** The better place for the extension is a singleton object because the uses can import the extension methods to bring
+  * them into lexical scope, which will make them applicable.
+  */
+object TwosComplementOps:
+  extension [N](n: N)(using tc: TwosComplement[N])
+    def isMinValue: Boolean = tc.equalsMinValue(n)
+    def absOption: Option[N] =
+      if !isMinValue then Some(tc.absOf(n)) else None
+    def negateOption: Option[N] =
+      if !isMinValue then Some(tc.negationOf(n)) else None
+
+/** Because Scala searches given instances for applicable extensions, thus the best home for these extensions is not in
+  * a singleton object like `TwosComplementOps`, but in the type class trait itself.
+  */
+
+/**
+  * Scala divides extension method search into two phases:
+	*	- first phase: looks in lexical scope:
+	* 	- defined directly
+	*   - imported
+	* 	- inherited
+	* - second phase: looks in three places:
+	*		- the members of the given instances in lexical scope
+	*		- the members of the companion objects of the receiver's class, super classes, and super traits
+	*		- the members of given instances in those same companion objects
+  */
 
 object TypedExtension extends App:
-	println(Byte.MinValue.absOption)
-	println(Short.MaxValue.negateOption)
-	println(-100L.absOption)
+  import TwosComplementOps.*
+
+  println(Byte.MinValue.absOption)
+  println(Short.MaxValue.negateOption)
+  println(-100L.absOption)
